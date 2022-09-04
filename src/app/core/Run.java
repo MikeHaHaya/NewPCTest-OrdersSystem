806bfeccf52997aa6b6275c172ff3346cbbc0c43
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-09-04 13:13:39 +0300

    OrderSystem, Run and singleton usage

---
+++ src/app/core/Run.java
@@ -0,0 +1,10 @@
+package app.core;
+
+import app.core.services.OrderSystem;
+
+public class Run {
+
+    public static void main(String[] args) {
+        OrderSystem.getInstance();
+    }
+}
---
+++ src/app/core/services/OrderSystem.java
@@ -0,0 +1,43 @@
+package app.core.services;
+
+import app.core.entities.Order;
+import app.core.threads.OrderTask;
+
+import java.util.Set;
+
+public final class OrderSystem {
+    // STATEMENTS FOR A SINGLETON CLASS
+
+    // Makes a static instance
+    private static OrderSystem INSTANCE;
+
+    // Creates the object
+    private OrderSystem() {}
+
+    // Returns the instance and creates an object if one does not exist
+    public static OrderSystem getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new OrderSystem();
+
+        return INSTANCE;
+    }
+
+    private Set<Order> orders;
+    private OrderTask task;
+
+    public boolean addOrder(Order order){
+        if (orders.add(order))
+            return true;
+        else
+            return false;
+    }
+
+    //TODO -- Return orders in a better way
+    public Set<Order> getOrders() {
+        return orders;
+    }
+
+    public void orderMenu() {
+
+    }
+}
