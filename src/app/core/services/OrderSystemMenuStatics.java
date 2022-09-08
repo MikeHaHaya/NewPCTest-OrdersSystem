5f2430c9a96478648426e51e00779d47059d549d
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-09-08 15:54:32 +0300

    For now, more menu.

--- src/app/core/services/OrderSystemMenuStatics.java
+++ src/app/core/services/OrderSystemMenuStatics.java
@@ -9,53 +9,62 @@ public class OrderSystemMenuStatics {
     protected static void greetings() {
 
         switch ((int) (Math.random() * 5)) {
-            case 0:
-                System.out.println("Welcome pedro, this is the new order what do you wish to order?");
-                break;
-            case 1:
-                System.out.println("Hello there, what do you wish to do?");
-                break;
-            case 2:
+            case 0 -> System.out.println("Welcome pedro, this is the new order what do you wish to order?");
+            case 1 -> System.out.println("Hello there, what do you wish to do?");
+            case 2 -> {
                 System.out.println("Hey Peter, how's your aunt?");
                 System.out.println("es una italiana muy caliente");
-                break;
-            case 3:
+            }
+            case 3 -> {
                 System.out.println("Hey, look who's here!");
                 System.out.print("Don't worry Zohan I tell you, they don't recognize you.");
-                break;
-            case 4:
-                System.out.println("Most test subjects do experience some cognitive deterioration after a few months in " +
-                        "suspension. Now you've been under for... quite a lot longer, and it's not out of the question " +
-                        "that you might have a very minor case of serious brain damage. But don't be alarmed, alright? " +
-                        "Although, if you do feel alarm, try to hold onto that feeling because that is the " +
-                        "proper reaction to being told you have brain damage.");
-                break;
+            }
+            case 4 ->
+                    System.out.println("Most test subjects do experience some cognitive deterioration after a few months in " +
+                            "suspension. Now you've been under for... quite a lot longer, and it's not out of the question " +
+                            "that you might have a very minor case of serious brain damage. But don't be alarmed, alright? " +
+                            "Although, if you do feel alarm, try to hold onto that feeling because that is the " +
+                            "proper reaction to being told you have brain damage.");
         }
     }
 
-    protected static void mainMenu() {
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println("To add order enter 1");
-        System.out.println("To view orders enter 2");
-        System.out.println("To quit the system enter 3");
+    protected static int mainMenu() {
 
-        // TODO -- Implement scanner correctly
-        String inStr;
-        int inInt;
-        inStr = scan.nextLine();
+        // TODO -- change if you can to uninitialized
+        int inInt = 0;
+        boolean valid = false;
 
-        try {
-            inInt = Integer.parseInt(inStr);
-        } catch (NumberFormatException e) {
-            System.out.println("Something went wrong, try again.");
-            // TODO -- Implement a system to send the user to type in again until he inserts a correct value
+        while (!valid) {
+
+
+            System.out.println(".");
+            System.out.println(".");
+            System.out.println(".");
+            System.out.println(".");
+            System.out.println(".");
+            System.out.println(".");
+            System.out.println("To add order enter 1");
+            System.out.println("To view orders enter 2");
+            System.out.println("To quit the system enter 3");
+
+            // TODO -- Implement scanner correctly
+            String inStr;
+
+            inStr = scan.nextLine();
+
+            try {
+                inInt = Integer.parseInt(inStr);
+                if (inInt == 1 || inInt == 2 || inInt == 3)
+                    valid = true;
+                else
+                    System.out.println("Don't know what that means, please enter 1, 2 or 3 to continue.");
+            } catch (NumberFormatException e) {
+                System.out.println("Something went wrong, try again.");
+                // TODO -- Implement a system to send the user to type in again until he inserts a correct value
+            }
+
         }
 
-
+        return inInt;
     }
 }
