85bdab29125f33fc1c38cefcd4fd5411d6c618b3
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-09-22 12:05:50 +0300

    Notify when an order has reached its deadline.

--- src/app/core/threads/OrderTask.java
+++ src/app/core/threads/OrderTask.java
@@ -2,11 +2,13 @@ package app.core.threads;
 
 import app.core.entities.Order;
 
+import java.util.Calendar;
 import java.util.Set;
 
 public class OrderTask implements Runnable{
 
     private Set<Order> orders;
+    Calendar now = Calendar.getInstance();
 
     public OrderTask(Set<Order> orders) {
         this.orders = orders;
@@ -14,13 +16,16 @@ public class OrderTask implements Runnable{
 
     @Override
     public void run() {
+
         while (true) {
-            try {
-                Thread.sleep(1000);
-                System.out.println("OrderTask Runs");
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+
+            for (Order order:orders) {
+                if (order.getReadyOn().before(now) && !order.isPopped()) {
+                    System.out.println(order.getName() + "has reached it's deadline!");
+                    order.setPopped(true);
+                }
             }
+
         }
     }
 
